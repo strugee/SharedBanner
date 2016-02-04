30049de81e3e5f6a28efcc4fb91d2de14239ef5d
Helen V. Holmes <helen.v.holmes@gmail.com>
2016-02-04 09:54:08 -0500

    Address @bwinton's inline comments

--- src/index.js
+++ src/index.js
@@ -15,14 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   head.appendChild(style);
-});
 
-var cb = function() {
   var l = document.createElement('link'); l.rel = 'stylesheet';
   l.href = 'https://code.cdn.mozilla.net/fonts/fira.css';
   var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-    webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(cb);
-else window.addEventListener('load', cb);
+});
